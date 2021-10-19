5bf7083f9b75284a4f65f1852ddbabaab78e1706
jiwon <90748096+qivvoon@users.noreply.github.com>
2021-10-19 17:44:01 +0900

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,9 @@
+// const imges = ["0.jpg" , "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+
+// const chosenImg = imges[Math.floor(Math.random() * imges.length)];
+
+// const bgImg = document.createElement("img");
+
+// bgImg.src = `img/${chosenImg}`;
+
+// document.body.(bgImg);
