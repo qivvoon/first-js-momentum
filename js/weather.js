a7ace3dc36560e5448b7c7f834c1a26708e58d0a
jiwon <90748096+qivvoon@users.noreply.github.com>
2021-10-24 13:35:03 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,6 @@
+const weather = document.querySelector("#weather");
 const city = document.querySelector("#weather span:first-child");
-const weather = document.querySelector("#weather span:last-child");   
+const degree = document.querySelector("#weather span:last-child");   
 const API_KEY = "632f02e1df38a5bf067021d0e47a8383";
 
 function onGeoOk(position) {
@@ -7,8 +8,9 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then((response) => response.json()).then((data) => {
-        city.innerText = data.name;
-        weather.innerText = `🍀 ${data.main.temp}℃ . ${data.weather[0].main}`;
+        city.innerText = data.name; 
+        degree.innerText = `🍀 ${data.main.temp}℃ . ${data.weather[0].main}`;
+        weather.classList.add("weatherani");
     });
 }
 function onGeoErr() {
